d083969ee5276381c192d4a34b7853e006cd7a00
Alejocram <alejocram@gmail.com>
2025-04-10 23:57:49 -0700

    Add a modal using Link of expo router

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
 const home = () => {
     return (
         <View style={styles.container}>
             <Text>Home</Text>
+            <Link href="/modal" style={styles.link}>
+                Open modal
+            </Link>
         </View>
     );
 };
@@ -16,6 +20,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#fff',
     },
+    link: {
+        paddingTop: 20,
+        fontSize: 20,
+        textDecorationLine: 'underline',
+    }
 });
 
 export default home;
---
+++ app/modal.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <View>
+      <Text>modal</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
+
+export default Modal;
